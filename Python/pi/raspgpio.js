152dd45aa711598bbb611d026f656349752908f0
sphnixdass <sphnixdass@gmail.com>
2020-01-07 01:08:14 +0530

    robojam

--- Python/pi/raspgpio.js
+++ Python/pi/raspgpio.js
@@ -1,7 +1,15 @@
 var Gpio = require('onoff').Gpio; //require onoff to control GPIO
-var LEDPin = new Gpio(24, 'out'); //declare GPIO4 an output
+var Pin1 = new Gpio(24, 'out'); //declare GPIO4 an output
+var Pin2 = new Gpio(23, 'out'); //declare GPIO4 an output
 var fs = require('fs'); //require filesystem to read html files
+var url = require('url');
+
 var http = require('http').createServer(function handler(req, res) { //create server
+
+  var path = url.parse(req.url).pathname;
+  console.log("css", path);
+  switch (path){
+    case '/':
   fs.readFile(__dirname + '/index.html', function (err, data) { //read html file
     if (err) {
       res.writeHead(500);
@@ -11,6 +19,75 @@ var http = require('http').createServer(function handler(req, res) { //create se
     res.writeHead(200);
     res.end(data);
   });
+  break;
+
+  case '/socket.io.js':
+  fs.readFile(__dirname + path, function (err, data) { //read html file
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading socket.io.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+  break;
+
+  case '/bootstrap.min.css':
+  fs.readFile(__dirname + path, function (err, data) { //read html file
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading socket.io.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+  break;
+
+  case '/jquery-3.2.1.slim.min.js':
+  fs.readFile(__dirname + path, function (err, data) { //read html file
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading socket.io.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+  break;
+
+  case '/popper.min.js':
+  fs.readFile(__dirname + path, function (err, data) { //read html file
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading socket.io.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+  break;
+
+  case '/bootstrap.min.js':
+  fs.readFile(__dirname + path, function (err, data) { //read html file
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading socket.io.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+  break;
+
+
+
+
+
+  }
+  
+
 });
 
 var io = require('socket.io')(http) //require socket.io module and pass the http object
@@ -20,10 +97,24 @@ http.listen(3010); //listen to port 8080
 io.sockets.on('connection', function (socket) {// WebSocket Connection
   var buttonState = 0; //variable to store button state
 
-  socket.on('state', function (data) { //get button state from client
-    buttonState = data;
-    if (buttonState != LEDPin.readSync()) { //Change LED state if button state is changed
-      LEDPin.writeSync(buttonState); //turn LED on or off
-    }
+  socket.on('forward', function (data) { //get button state from client
+    console.log("forward pressed")
+      Pin1.writeSync(0); //turn LED on or off
+      Pin2.writeSync(1);
+   
   });
+
+  socket.on('state', function (data) { //get button state from client
+    Pin1.writeSync(0); //turn LED on or off
+    Pin2.writeSync(0);
+ 
+  });
+
+  socket.on('backward', function (data) { //get button state from client
+    Pin1.writeSync(1); //turn LED on or off
+    Pin2.writeSync(0);
+ 
+  });
+
+
 });
